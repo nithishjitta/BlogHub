1dcde232b5316a3c1b713211ab01dc108b6dd3c6
Manish <nithish3046@gmail.com>
2026-05-16 20:40:00 +0530

    Updated css

--- client/src/AuthContext.jsx
+++ client/src/AuthContext.jsx
@@ -6,6 +6,8 @@ const API = import.meta.env.VITE_API_URL;
 
 axios.defaults.withCredentials = true;
 
+export const useAuth = () => useContext(AuthContext);
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
